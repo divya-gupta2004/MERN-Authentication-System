5f9fed5eb0ddcc0a9ada3815dfc9518550224bf7
divya-gupta2004 <divyagupta20042004@gmail.com>
2025-09-19 04:21:50 +0530

    Fix logout, CORS, and cookie handling

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
-const API_URL = "https://mern-authentication-backend-01mj.onrender.com/api/v1";
+// const API_URL = "https://mern-authentication-backend-01mj.onrender.com/api/v1";
+const API_URL = import.meta.env.VITE_API_URL;
 // For local development: "http://localhost:4001/api/v1"
 
 export default API_URL;
