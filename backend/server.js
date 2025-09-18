3920495be89b20633b8002e3d8be6d5bcc9b37c1
divya-gupta2004 <divyagupta20042004@gmail.com>
2025-09-19 02:38:36 +0530

    Updated server.js to use dynamic port with dotenv

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ config({ path: "./config.env" });
 // console.log("TWILIO_SID loaded:", Boolean(process.env.TWILIO_SID));
 // console.log("TWILIO_AUTH_TOKEN loaded:", Boolean(process.env.TWILIO_AUTH_TOKEN));
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening on ${process.env.PORT}`);
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
+
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server listening on ${process.env.PORT}`);
+// });
